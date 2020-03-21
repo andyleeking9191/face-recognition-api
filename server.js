4ad9b6e365b61527a8b88a41e5ff801206ef6ef9
andyleeking9191 <andyleeking9191@users.noreply.github.com>
2020-03-20 21:05:29 -0400

    updated database to be compliant with heroku

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-animated-99938',
-      user : 'postgres',
-      password : '',
-      database : 'smart_brain'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
   });
 
