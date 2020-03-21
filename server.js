fcdc3b271b584e4cfe3b88151802cdf95e311ea0
andyleeking9191 <andyleeking9191@users.noreply.github.com>
2020-03-20 22:14:36 -0400

    removed typo

--- server.js
+++ server.js
@@ -19,14 +19,14 @@ const db = knex({
     }
   });
 
-app.use(cors());
+app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {res.send('it is working!')});
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)});
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
-app.put('/image', (req, res) => { image.handleImagePut(req, res, db)});
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
+app.get('/', (req, res) => {res.send('it is working!')})
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(PORT || 3001, () => console.log(`The server is running on port ${PORT}.`));
